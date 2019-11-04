109bd47acb84f69f6bd83fd22c0c559824c98c4d
Samikshya <samikshyaaryal5@gmail.com>
2019-11-04 21:19:13 +0200

    connecting DB

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-
-const cors = reqire ('cors'); 
-
-
-require('dotenv').config(); // dotenv vanne env variable configure garxa
-
-const app  = express();   // yeha express server create hunxa, 
-const  port = process.env.PORT || 5000; 
+const app= express();
+const port  = process.env.PORT || 5000;
 
 app.use(cors()); // middleware
 app.use(express.json()); // this allows to parsse the json
 
+const uri = process.env.ATLAS_URI;
+mangoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+
+
+const connection = mongoose.connection; 
+connection.once('open', () => {
+    console.log("Successfull");
+})
+
 app.listen(port, () => {
-   console.log('server is running on port : ${port}');
+   console.log(`server is running on port : ${port}`);
 });
