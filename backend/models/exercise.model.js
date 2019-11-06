b731b92adfb255141372e4c24f4d80cdc9eb5874
Samikshya <samikshyaaryal5@gmail.com>
2019-11-06 21:36:34 +0200

    adding routes folder

--- backend/models/exercise.model.js
+++ backend/models/exercise.model.js
@@ -5,11 +5,11 @@ const Schema = mongoose.Schema;
 const exerciseSchema = new Schema ({
     username: {type: string, required: true},
     description: {type: string, required: true},
-    duration: {type: string, required: true},
-    date: {type: string, required: true},
+    duration: {type: Number, required: true},
+    date: {type: Date, required: true},
 }, {
     timestamps: true,
 
 });
-const User = mongoose. model('Exercise', exerciseSchema);
-module.exports = User;
+const Exercise = mongoose. model('Exercise', exerciseSchema);
+module.exports = Exercise;
--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,8 @@ connection.once('open', () => {
 const exerisesRouter= require('./routes.exercises');
 const usersRouter = require('./routes/users');
 
-app.use('exercise', exerisesRouter);
-app.use('users', usersRouter);
+app.use('/exercise', exerisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
    console.log(`server is running on port : ${port}`);
